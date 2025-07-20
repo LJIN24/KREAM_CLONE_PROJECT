4b3d988869bf5dec1f11f94f3370ee31d91407c9
JIN <jin724596@gmail.com>
2025-07-20 22:12:18 +0900

    Comment Data Equtable 추가

--- Kreamclone/Model/Comment.swift
+++ Kreamclone/Model/Comment.swift
@@ -10,7 +10,7 @@ import Foundation
 import Firebase
 import FirebaseFirestore
 
-struct Comment: Identifiable, Decodable {
+struct Comment: Identifiable, Decodable, Equatable {
     @DocumentID var id: String?
     let comment: String
     let username: String
@@ -18,5 +18,6 @@ struct Comment: Identifiable, Decodable {
     var commenterImageUrl: String?
     let postId:  String
     let timestamp: Timestamp
+    
 }
 
